d80aee799996e53a2edc64c01730da6760f9bfc4
nayan <nayannjddas@gmail.com>
2024-04-05 17:13:20 +0530

    Connect mongoDB and run express server

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import app from "./app.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Server failed! ", error);
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("mongoDB connection failed!!! \n Details: ", error);
+  });
