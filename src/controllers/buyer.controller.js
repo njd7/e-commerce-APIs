c524382352924fc25f63c6016c6113118551118e
nayan <nayannjddas@gmail.com>
2024-04-06 20:52:46 +0530

    buyer and seller controllers updated

--- src/controllers/buyer.controller.js
+++ src/controllers/buyer.controller.js
@@ -9,129 +9,151 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const getSellers = asyncHandler(async (req, res) => {
-  const allSellers = [];
+  try {
+    const allSellers = [];
 
-  const sellers = await User.find({ userType: "SELLER" });
-  for (let seller of sellers) {
-    const sellerObject = {
-      username: "",
-      fullName: "",
-      email: "",
-      seller_id: "",
-      catalog: [],
-    };
-    sellerObject.username = seller.username;
-    sellerObject.fullName = seller.fullName;
-    sellerObject.email = seller.email;
-    sellerObject.seller_id = seller._id;
+    const sellers = await User.find({ userType: "SELLER" });
 
-    const productList = [];
+    for (let seller of sellers) {
+      const sellerObject = {
+        username: seller.username,
+        fullName: seller.fullName,
+        email: seller.email,
+        seller_id: seller._id,
+        catalog: [],
+      };
+      const sellerProducts = await Catalog.aggregate([
+        { $match: { owner: seller._id } },
+        {
+          $lookup: {
+            from: "catalogproducts",
+            localField: "_id",
+            foreignField: "catalog",
+            as: "catalogProducts",
+          },
+        },
+        {
+          $unwind: "$catalogProducts",
+        },
+        {
+          $lookup: {
+            from: "products",
+            localField: "catalogProducts.product",
+            foreignField: "_id",
+            as: "product",
+          },
+        },
+        {
+          $project: {
+            product: { $arrayElemAt: ["$product", 0] },
+          },
+        },
+      ]);
 
-    const catalog = await Catalog.findOne({ owner: seller._id });
+      sellerObject.catalog = sellerProducts.map(({ product }) => ({
+        name: product.name,
+        price: product.price,
+      }));
 
-    if (catalog) {
-      const catalogId = catalog._id;
-
-      const catalogProducts = await CatalogProduct.find({ catalog: catalogId });
-
-      for (let item of catalogProducts) {
-        const product = await Product.findById(item.product).select(
-          "name price"
-        );
-
-        productList.push(product);
-      }
+      allSellers.push(sellerObject);
     }
-    sellerObject.catalog = productList;
 
-    allSellers.push(sellerObject);
+    return res
+      .status(200)
+      .json(new ApiResponse(200, allSellers, "List of sellers fetched"));
+  } catch (error) {
+    throw new ApiError(500, "Internal Server Error");
   }
-
-  return res
-    .status(200)
-    .json(new ApiResponse(200, allSellers, "List of sellers fetched"));
 });
 
 const getSellerCatalog = asyncHandler(async (req, res) => {
-  const { seller_id } = req.params;
-  const seller = await User.findById(seller_id);
-  if (!seller) {
-    throw new ApiError(404, "Invalid id");
-  }
-  if (seller.userType !== "SELLER") {
-    throw new ApiError(400, "Provided ID does not correspond to a seller");
-  }
-  const catalogObject = { seller_id, count: 0, catalog: [] };
-  const productList = [];
+  try {
+    const { seller_id } = req.params;
 
-  const catalog = await Catalog.findOne({ owner: seller_id });
-
-  if (catalog) {
-    const catalogId = catalog._id;
-    catalogObject.count = catalog.count;
-
-    const catalogProducts = await CatalogProduct.find({ catalog: catalogId });
-
-    for (let item of catalogProducts) {
-      const product = await Product.findById(item.product).select("name price");
-
-      productList.push(product);
+    const seller = await User.findOne({ _id: seller_id, userType: "SELLER" });
+    if (!seller) {
+      throw new ApiError(404, "Seller not found");
     }
+
+    const catalog = await Catalog.findOne({ owner: seller_id }).select("count");
+    const catalogObject = { seller_id, count: 0, catalog: [] };
+
+    if (catalog) {
+      catalogObject.count = catalog.count;
+
+      const catalogProducts = await CatalogProduct.find({
+        catalog: catalog._id,
+      })
+        .select("product")
+        .populate("product", "name price");
+
+      catalogObject.catalog = catalogProducts.map(({ product }) => product);
+    }
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, catalogObject, "Seller catalog fetched"));
+  } catch (error) {
+    throw new ApiError(500, "Internal Server Error");
   }
-
-  catalogObject.catalog = productList;
-
-  return res
-    .status(200)
-    .json(new ApiResponse(200, catalogObject, "Seller catalog fetched"));
 });
 
 const createOrder = asyncHandler(async (req, res) => {
-  const { seller_id } = req.params;
-  const { itemsList } = req.body;
+  try {
+    const { seller_id } = req.params;
+    const { itemsList } = req.body;
 
-  if (!itemsList) {
-    throw new ApiError(400, "Please provide itemsList");
-  }
-
-  // Check if all the item has both units and product properties
-  for (let item of itemsList) {
-    if (!item.hasOwnProperty("units") || !item.hasOwnProperty("product")) {
-      throw new ApiError(
-        400,
-        "Please provide units and product for each order item"
-      );
+    // validate itemsList
+    if (!itemsList) {
+      throw new ApiError(400, "Please provide itemsList");
     }
+
+    // Check if all the item has both units and product properties
+    for (let item of itemsList) {
+      if (!item.hasOwnProperty("units") || !item.hasOwnProperty("product")) {
+        throw new ApiError(
+          400,
+          "Please provide units and product for each order item"
+        );
+      }
+    }
+
+    // create order
+    const order = await Order.create({
+      buyer: req?.user?._id,
+      seller: seller_id,
+    });
+
+    if (!order) {
+      throw new ApiError(500, "Something went wrong while creating order");
+    }
+
+    const orderItemsToInsert = itemsList.map((item) => {
+      return { ...item, order: order._id };
+    });
+
+    const orderItems = await OrderItem.insertMany(orderItemsToInsert);
+
+    if (!orderItems) {
+      throw new ApiError(500, "Something went wrong while creating OrderItems");
+    }
+
+    // Calculate total order price
+    let total = 0;
+    for (let item of orderItems) {
+      const product = await Product.findById(item.product);
+      total += product.price * item.units;
+    }
+
+    // Update order price
+    order.orderPrice = total;
+    await order.save({ validateBeforeSave: false });
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, {}, "Order created successfully"));
+  } catch (error) {
+    throw new ApiError(500, "Internal Server Error");
   }
-
-  const order = await Order.create({
-    buyer: req?.user?._id,
-    seller: seller_id,
-  });
-
-  if (!order) {
-    throw new ApiError(500, "Something went wrong while creating order");
-  }
-
-  const orderItemsToInsert = itemsList.map((item) => {
-    return { ...item, order: order._id };
-  });
-
-  const orderItems = await OrderItem.insertMany(orderItemsToInsert);
-  if (!orderItems) {
-    throw new ApiError(500, "Something went wrong while creating OrderItems");
-  }
-
-  let total = 0;
-  for (let item of orderItems) {
-    total += (await Product.findById(item.product)).price * item.units;
-  }
-
-  order.orderPrice = total;
-  await order.save({ validateBeforeSave: false });
-
-  return res
-    .status(200)
-    .json(new ApiResponse(200, {}, "Order created successfully"));
 });
 export { getSellers, getSellerCatalog, createOrder };
--- src/controllers/seller.controller.js
+++ src/controllers/seller.controller.js
@@ -6,101 +6,115 @@ import { Product } from "../models/ecommerce/product.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import mongoose from "mongoose";
 
 const createCatalog = asyncHandler(async (req, res) => {
   // seller_id retrieved from auth middleware verifyJWT
-  // create catalog
-  const { products } = req.body;
-  if (!products) {
-    throw new ApiError(400, "Products required");
-  }
 
-  // check if Catalog is already created
-  let catalog = await Catalog.findOne({ owner: req?.user?._id });
-  const catalogStatus = !catalog ? "created" : "updated";
-
-  if (!catalog) {
-    catalog = await Catalog.create({
-      owner: req?.user?._id,
-      count: products.length,
-    });
-    if (!catalog) {
-      throw new ApiError(500, "Something went wrong while creating catalog");
+  try {
+    const { products } = req.body;
+    if (!products) {
+      throw new ApiError(400, "Products required");
     }
-  } else {
-    catalog.count += products.length;
-    await catalog.save({ validateBeforeSave: false });
+
+    // Check if Catalog is already created
+    let catalog = await Catalog.findOne({ owner: req?.user?._id });
+    const catalogStatus = !catalog ? "created" : "updated";
+
+    // Create or update catalog
+    if (!catalog) {
+      catalog = await Catalog.create({
+        owner: req?.user?._id,
+        count: products.length,
+      });
+      if (!catalog) {
+        throw new ApiError(500, "Something went wrong while creating catalog");
+      }
+    } else {
+      catalog.count += products.length;
+      await catalog.save({ validateBeforeSave: false });
+    }
+
+    const productsToInsert = products.map((product) => {
+      return { product, catalog: catalog._id };
+    });
+
+    const catalogProducts = await CatalogProduct.insertMany(productsToInsert);
+
+    if (!catalogProducts) {
+      throw new ApiError(
+        500,
+        "Something went wrong while creation catalogProducts"
+      );
+    }
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, {}, `Catalog ${catalogStatus} successfully`));
+  } catch (error) {
+    throw new ApiError(500, "Internal Server Error");
   }
-
-  const productsToInsert = products.map((product) => {
-    return { product, catalog: catalog._id };
-  });
-
-  const catalogProducts = await CatalogProduct.insertMany(productsToInsert);
-  if (!catalogProducts) {
-    throw new ApiError(
-      500,
-      "Something went wrong while creation catalogProducts"
-    );
-  }
-
-  return res
-    .status(200)
-    .json(new ApiResponse(200, {}, `Catalog ${catalogStatus} successfully`));
 });
 
 const getOrders = asyncHandler(async (req, res) => {
-  const allOrders = [];
-  const orders = await Order.find({ seller: req?.user?._id });
+  try {
+    const allOrders = [];
+    const orders = await Order.find({ seller: req?.user?._id });
 
-  for (let order of orders) {
-    const orderObject = {
-      buyer: order.buyer,
-      seller: order.seller,
-      orderPrice: order.orderPrice,
-      orderStatus: order.orderStatus,
-      deliveryStatus: order.deliveryStatus,
-      isPaymentDone: order.isPaymentDone,
-      items: [],
-    };
+    for (let order of orders) {
+      const orderObject = {
+        buyer: order.buyer,
+        seller: order.seller,
+        orderPrice: order.orderPrice,
+        orderStatus: order.orderStatus,
+        deliveryStatus: order.deliveryStatus,
+        isPaymentDone: order.isPaymentDone,
+        items: [],
+      };
 
-    const ordersList = [];
-    const orderItems = await OrderItem.find({ order: order._id }).select(
-      "product units"
-    );
-    for (let item of orderItems) {
-      const product = await Product.findById(item.product).select("name");
-      ordersList.push({ product, units: item.units });
+      const orderItems = await OrderItem.find({ order: order._id }).select(
+        "product units"
+      );
+      for (let item of orderItems) {
+        const product = await Product.findById(item.product).select("name");
+        orderObject.items.push({ product, units: item.units });
+      }
+
+      allOrders.push(orderObject);
     }
-    orderObject.items = ordersList;
 
-    allOrders.push(orderObject);
+    return res
+      .status(200)
+      .json(new ApiResponse(200, allOrders, "Orders fetched"));
+  } catch (error) {
+    throw new ApiError(500, "Internal Server Error");
   }
-
-  return res
-    .status(200)
-    .json(new ApiResponse(200, allOrders, "Orders fetched"));
 });
 
 const createProduct = asyncHandler(async (req, res) => {
-  const { name, price } = req.body;
-  if (!name || !price) {
-    throw new ApiError(400, "Both name and price required!");
+  try {
+    const { name, price } = req.body;
+
+    // Validate name and price
+    if (!name || !price) {
+      throw new ApiError(400, "Both name and price required!");
+    }
+
+    // Create product
+    const product = await Product.create({
+      name,
+      price,
+    });
+
+    if (!product) {
+      throw new ApiError(500, "Something went wrong while creating product");
+    }
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, product, "Product created succcessfully"));
+  } catch (error) {
+    throw new ApiError(500, "Internal Server Error");
   }
-
-  const product = await Product.create({
-    name,
-    price,
-  });
-
-  if (!product) {
-    throw new ApiError(500, "Something went wrong while creating product");
-  }
-
-  return res
-    .status(200)
-    .json(new ApiResponse(200, product, "Product created succcessfully"));
 });
 
 export { createCatalog, getOrders, createProduct };
